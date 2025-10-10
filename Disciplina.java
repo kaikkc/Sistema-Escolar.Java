14d49f04d82ffe8306a3514e27520f9fc0b514cf
Ik7Mt0s <icaromatoscb@gmail.com>
2025-10-10 10:45:28 -0300

    Construtor da disciplina

--- Disciplina.java
+++ Disciplina.java
@@ -1,4 +1,9 @@
 public class Disciplina {
     private String codigo;
     private String nome;
+
+    public Disciplina(String codigo, String nome) {
+        this.codigo = codigo;
+        this.nome = nome;
+    }
 }
